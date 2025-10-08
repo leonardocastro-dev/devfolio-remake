0e2a99c7d12dda5f36194b7315db223c57bc2cb1
Leonardo de Castro <leonardo-programmer@proton.me>
2025-10-08 17:17:18 -0300

    Fix adjust main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       >
         <Navigation />
         <TooltipProvider>
-          <main>{children}</main>
+          <main className="flex-1 overflow-hidden">{children}</main>
         </TooltipProvider>
         <Footer />
       </body>
