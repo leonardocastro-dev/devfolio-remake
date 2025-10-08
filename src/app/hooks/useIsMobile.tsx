dedfa9fb3b61972d892d71dd3c1c85cf8d38bd18
Leonardo de Castro <leonardo-programmer@proton.me>
2025-10-08 16:47:49 -0300

    Fix add react-responsive

---
+++ src/app/hooks/useIsMobile.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import { useMediaQuery } from 'react-responsive'
+
+export function useIsMobile() {
+  return useMediaQuery({ query: '(max-width: 1024px)' })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 import SnakeGame from '@/components/layout/snake-game'
 import TextType from '@/components/ui/text-type'
+import { useIsMobile } from './hooks/useIsMobile'
 
 export default function Home() {
+  const isMobile = useIsMobile()
+
   return (
     <section className="flex flex-col pt-24 pb-20 lg:py-0 justify-center items-center overflow-hidden px-8 h-full">
       <div className="flex max-w-7xl w-full items-center lg:justify-between justify-center flex-grow gap-8">
@@ -22,10 +27,10 @@ export default function Home() {
 
           <div className="lg:space-y-2 space-y-4 lg:mt-20">
             <p className="lg:block hidden text-muted-foreground">
-              {'// complete the game to continue'}
+              // complete the game to continue
             </p>
             <p className="text-muted-foreground">
-              {'// you can also see it on my Github page'}
+              {isMobile ? '// find my profile on Github:' : '// you can also see it on my Github page'}
             </p>
             <p className="font-medium">
               <span className="text-chart-1">const</span>{' '}
