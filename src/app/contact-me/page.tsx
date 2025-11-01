43a697c6897f13ede8e9d5f4927035c8cc493720
Leonardo de Castro <leonardo-programmer@proton.me>
2025-11-01 12:40:58 -0300

    Fix responsive contacts

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -5,11 +5,16 @@ import SidebarSection from '@/components/layout/ide-sidebar/components/sidebar-s
 import Icon from '@/components/ui/icon'
 import ContactForm from './components/contact-form'
 import ContactCode from './components/contact-code'
+import { Activity } from 'react'
+import { useIsMobile } from '../hooks/useIsMobile'
 
 export default function ContactMe() {
+  const isMobile = useIsMobile()
+
   return (
-    <section className="flex h-full">
-      <div className="flex max-w-xs w-full h-full">
+    <section className="flex flex-col lg:flex-row h-full">
+      <h2 className="lg:hidden px-7 pb-7 pt-5 text-sm">_contact-me</h2>
+      <div className="flex lg:max-w-xs w-full">
         <IdeSidebar>
           <SidebarSection label="contacts">
             <div className="flex flex-col gap-2.5 px-3.5 py-2.5">
@@ -28,10 +33,12 @@ export default function ContactMe() {
         </IdeSidebar>
       </div>
       <div className="w-full flex flex-col h-full">
-        <nav className="overflow-hidden min-h-10 relative border-b border-primary-200"></nav>
-        <div className="grid grid-cols-2 h-full overflow-hidden">
+        <nav className="hidden lg:block overflow-hidden min-h-10 relative border-b border-primary-200"></nav>
+        <div className="lg:grid grid-cols-2 h-full overflow-hidden">
           <ContactForm />
-          <ContactCode />
+          <Activity mode={isMobile ? 'hidden' : 'visible'}>
+            <ContactCode />
+          </Activity>
         </div>
       </div>
     </section>
