baf57563195b636f55e553c9ec73ca4266730538
Leonardo de Castro <leonardo-programmer@proton.me>
2025-11-12 15:52:55 -0300

    Fix adjust basepath project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   basePath: '/devfolio',
+  assetPrefix: '/devfolio/',
 }
 
 export default nextConfig
